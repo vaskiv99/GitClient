14dac711d755268c7d6801a72e22f0a9b1d130d8
Vasul <vaskiv99@ukr.net>
2018-06-25 10:44:00 +0300

    Small changes

--- MyGitClient/MainWindow.xaml.cs
+++ MyGitClient/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System.Windows;
-using System.Windows.Input;
 using MyGitClient.ViewModels;
 namespace MyGitClient
 {
     public partial class MainWindow : Window
     {
-        private MainWindowViewModel _mainWindow;
-
         public MainWindow()
         {
-            _mainWindow = new MainWindowViewModel();
             InitializeComponent();
-            DataContext = _mainWindow;
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ MyGitClient/View/CommitWIndow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyGitClient.ViewModels;
+using System;
+using System.Windows;
+
+namespace MyGitClient
+{
+    public partial class CommitWindow : Window
+    {
+        public CommitWindow(Guid id)
+        {
+            InitializeComponent();
+            DataContext = new CommitViewModel(id);
+        }
+    }
+}
